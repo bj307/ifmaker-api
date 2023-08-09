d3d07a684fd4e9a71686e50c71163edea9455488
kaio <kaioresendeoficial@outlook.com>
2023-08-09 15:08:47 -0300

    :construcion: crud projetos :construction:

--- src/projeto/projeto.controller.ts
+++ src/projeto/projeto.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, Request } from '@nestjs/common';
+import { ProjetoService } from './projeto.service';
+import { ProjetoDTO } from './DTO/projeto.dto';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtPayload } from 'src/auth/model/jwtpayload.model';
+import { verify } from 'jsonwebtoken';
 
 @Controller('projeto')
-export class ProjetoController {}
+export class ProjetoController {
+  constructor(
+    private readonly projetoService: ProjetoService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('novo')
+  public async cadastrar(
+    @Request() req,
+    @Body() p: ProjetoDTO,
+  ): Promise<ProjetoDTO> {
+    const jwtToken = await this.authService.jwtExtractor(req);
+    const jwtPay = verify(jwtToken, process.env.JWT_SECRET) as JwtPayload;
+    p.usuarios.push(jwtPay.userId);
+    return await this.projetoService.cadastrar(p);
+  }
+
+  @Get('id')
+  public async buscarID(@Param('id') id: string): Promise<ProjetoDTO> {
+    const projeto = await this.projetoService.buscarID(id);
+    if (!projeto) {
+      return;
+    }
+
+    return projeto;
+  }
+}
--- src/projeto/projeto.module.ts
+++ src/projeto/projeto.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjetoService } from './projeto.service';
 import { ProjetoController } from './projeto.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [ProjetoService],
-  controllers: [ProjetoController]
+  controllers: [ProjetoController],
 })
 export class ProjetoModule {}
